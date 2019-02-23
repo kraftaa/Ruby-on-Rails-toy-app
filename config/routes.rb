c08b83bd83d8fcc73185b9343929e9897fdf2d0e
kraftaa <kraftaa@gmail.com>
2019-02-23 15:20:25 -0800

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
+# Rails.application.routes.draw do
+#   resources :microposts
+#   resources :users
+#   root 'users#index'
+# end
+#
+#
 Rails.application.routes.draw do
+
+
+
+  root 'application#hello'
   resources :microposts
   resources :users
+  resources :static_pages
   root 'users#index'
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help', as: 'helf'
+  get  '/about',   to: 'static_pages#about'
+  get  '/home', to: 'static_pages#home'
 end
